877ffc7951fed730685d01b0ffbd812c8aa676a9
albertoesmp <albertoesmp@gmail.com>
2023-07-31 13:56:00 +0200

    Documented pre/post processing for PNet.

--- src/model/deeplearn/dlrun/point_net_post_processor.py
+++ src/model/deeplearn/dlrun/point_net_post_processor.py
@@ -12,18 +12,37 @@ class PointNetPostProcessor:
     Postprocess the output of a PointNet neural network to transform it to the
     expected output format.
 
-    # TODO Rethink : Sphinx doc @ member attributes
+    :ivar pnet_preproc: The preprocessor that generated the input for the model
+        which output must be handled by the post-processor.
     """
     # ---   INIT   --- #
     # ---------------- #
     def __init__(self, pnet_preproc, **kwargs):
+        """
+        Initialization/instantiation of a PointNet post-processor.
+
+        :param pnet_preproc: The pre-processor associated to the model which
+            output must be handled by the post-processor.
+        :type pnet_preproc: :class:`.PointNetPreProcessor`
+        :param kwargs: The key-word arguments for the PointNetPostProcessor.
+        """
         # Assign attributes
         self.pnet_preproc = pnet_preproc  # Corresponding pre-processor
 
     # ---   RUN/CALL   --- #
     # -------------------- #
     def __call__(self, inputs):
-        # TODO Rethink : Sphinx doc
+        """
+        Executes the post-processing logic.
+
+        :param inputs: A key-word input where the key "X" gives the coordinates
+        of the points in the original point cloud. Also, the key "z" gives the
+        predictions computed on a receptive field of :math:`R` points that must
+        be propagated back to the :math:`m` points of the original point cloud.
+        :type inputs: dict
+        :return: The :math:`m` point-wise predictions derived from the
+            :math:`R` input predictions on the receptive field.
+        """
         # Extract inputs
         X = inputs['X']  # The original point cloud (before receptive field)
         z_reduced = inputs['z']  # Softmax scores reduced to receptive field
@@ -41,5 +60,6 @@ class PointNetPostProcessor:
             z[I[i]] += z_prop_i
             count[I[i]] += 1
         z = z / count if len(z.shape) < 2 else (z.T/count).T
+        z = np.sum(z, axis=1)
         # Return
         return z
--- src/model/deeplearn/dlrun/point_net_pre_processor.py
+++ src/model/deeplearn/dlrun/point_net_pre_processor.py
@@ -1,6 +1,7 @@
 # ---   IMPORTS   --- #
 # ------------------- #
 from src.utils.receptive_field import ReceptiveField
+import src.main.main_logger as LOGGING
 import numpy as np
 from scipy.spatial import KDTree as KDT
 import scipy.stats
@@ -9,21 +10,58 @@ import scipy.stats
 # ---   CLASS   --- #
 # ----------------- #
 class PointNetPreProcessor:
-    """
+    r"""
     :author: Alberto M. Esmoris Pena
 
     Preprocess the input dictionary of X (coordinates), F (features), and y
     (expected values) so it can be feed into the PointNet neural network.
 
-    # TODO Rethink : Sphinx doc @ member attributes
+    :ivar sphere_radius: The radius of the sphere that bounds a neighborhood.
+        For an arbitrary set of 3D points it can be found as follows:
+
+        .. math::
+            r = \dfrac{1}{2} \, \max \; \biggl\{
+                x_{\mathrm{max}}-x_{\mathrm{min}},
+                y_{\mathrm{max}}-y_{\mathrm{min}},
+                z_{\mathrm{max}}-z_{\mathrm{min}}
+            \biggr\}
+
+    :vartype sphere_radius: float
+    :ivar separation_factor: How many times the sphere radius separates the
+        support points to find the input neighborhoods that will be used to
+        generate the receptive fields for the neural network.
+
+        For a given separation factor :math:`k`, the following condition
+        should be satisfied to prevent missing any region of the input
+        point cloud on a :math:`n`-dimensional space:
+
+        .. math::
+            k \leq \dfrac{2}{\sqrt{n}}
+
+    :vartype separation_factor: float
+    :ivar cell_size: The cell size defining the receptive field. See
+        :class:`ReceptiveField`.
+    :vartype cell_size: :class:`np.ndarray`
+    :ivar last_call_receptive_fields: List of the receptive fields used the
+        last time that the pre-processing logic was executed.
+    :vartype last_call_receptive_fields: list
+    :ivar last_call_neighborhoods: List of neighborhoods (represented by
+        indices) used the last time that the pre-processing logic was
+        executed.
+    :vartype last_call_neighborhoods: list
     """
     # ---   INIT   --- #
     # ---------------- #
     def __init__(self, **kwargs):
+        """
+        Initialization/instantiation of a PointNet pre-processor.
+
+        :param kwargs: The key-word arguments for the PointNetPreProcessor.
+        """
         # Assign attributes
         self.sphere_radius = kwargs.get('sphere_radius', 1.0)
         self.separation_factor = kwargs.get('separation_factor', np.sqrt(3)/4)
-        self.separation_factor = np.sqrt(3)/2  # TODO Remove
+        self.separation_factor = 1  # TODO Remove
         self.cell_size = np.array(kwargs.get('cell_size', [0.1, 0.1, 0.1]))
         # Initialize last call cache
         self.last_call_receptive_fields = None
@@ -32,7 +70,43 @@ class PointNetPreProcessor:
     # ---   RUN/CALL   --- #
     # -------------------- #
     def __call__(self, inputs):
-        # TODO Rethink : Sphinx doc
+        """
+        Executes the pre-processing logic. It also updates the cache-like
+        variables of the preprocessor.
+
+        The pre-processing logic consists of steps:
+
+        **1) Generate support points.**
+
+        Support points are generated as a set of points separated in :math:`kr`
+        units between them and covering the entire point cloud. Watch out,
+        generating support points with
+        :math:`k > 2/\sqrt{n}` in a :math:`n`-dimensional
+        space leads to gaps between the bounding spheres centered on the
+        support points.
+
+        **2) Find neighborhoods centered on support points.**
+        For each support point, a neighborhood (by default a spherical
+        neighborhood but the neighborhood definition can be arbitrarily
+        changed) centered on it is found.
+
+        **3) Filter empty neighborhoods.**
+        Any support point that leads to an empty neighborhood when considering
+        as neighbors points from the input point cloud is filtered out.
+
+        **4) Trasnform non-empty neighborhoods to receptive fields.**
+        Each non-empty neighborhood is transformed to a receptive field.
+        See :class:`.ReceptiveField` for further details.
+
+        :param inputs: A key-word input where the key "X" gives the input
+            dataset and the "y" (OPTIONALLY) gives the reference values that
+            can be used to fit/train a PointNet model.
+        :type inputs: dict
+        :return: Either (Xout, yout) or Xout. Where Xout are the points
+            representing the receptive field and yout (only given when
+            "y" was given in the inputs dictionary) the corresponding
+            reference values for those points.
+        """
         # Extract inputs
         X, y = inputs['X'], inputs.get('y', None)
         # Build support points
@@ -53,7 +127,7 @@ class PointNetPreProcessor:
         non_empty_mask = [len(Ii) > 0 for Ii in I]
         I = [Ii for i, Ii in enumerate(I) if non_empty_mask[i]]
         sup_X = sup_X[non_empty_mask]
-        self.last_call_neighborhoods = [Ii for Ii in I]
+        self.last_call_neighborhoods = [Ii for Ii in I]  # TODO Restore or Rethink
         # Prepare receptive field
         self.last_call_receptive_fields = [
             ReceptiveField(
@@ -72,6 +146,10 @@ class PointNetPreProcessor:
             )
             for i, Ii in enumerate(I)
         ])
+        LOGGING.LOGGER.info(
+            f'The point net pre processor generated {Xout.shape[0]} receptive '
+            'fields. '
+        )
         if y is not None:
             yout = np.array([
                 self.last_call_receptive_fields[i].reduce_values(
@@ -80,7 +158,6 @@ class PointNetPreProcessor:
                     reduce_f=lambda x: scipy.stats.mode(x)[0][0],
                     fill_nan=True
                 ) for i, Ii in enumerate(I)
-            ])  # TODO Rethink : New
-            print(f'Num training cases: {len(yout)}')  # TODO Remove
+            ])
             return Xout, yout
         return Xout
